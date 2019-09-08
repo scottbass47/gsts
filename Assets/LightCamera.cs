993852641db7444a83acad19d244755703128cae
Scott Bass <scottbass47@gmail.com>
2019-09-08 16:57:28 -0400

    Test

---
+++ Assets/LightCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCamera : MonoBehaviour
+{
+    [SerializeField] private Transform gameCamera;
+
+    private void Start()
+    {
+        GetComponent<Camera>().orthographicSize = GameSettings.Settings.OrthographicZoom;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = gameCamera.position;
+    }
+}
